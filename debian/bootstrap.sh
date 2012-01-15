0154103f1d62438898742af5e2698c07d277ec64
Stefan Lippers-Hollmann <s.l-h@gmx.de>
2012-01-15 23:17:21 +0000

    switch to compat level 9.

--- debian/bootstrap.sh
+++ debian/bootstrap.sh
@@ -1,4 +1,16 @@
 #!/bin/sh
+#
+# Copyright © 2010-2012 Stefan Lippers-Hollmann <s.l-h@gmx.de>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 2 of 
+# the License only.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
 
 set -e
 
